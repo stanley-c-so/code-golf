ec20e2a3015484388b56e6be4b27ba5f25424dd1
stanley-c-so <stanley.c.so@gmail.com>
2019-09-27 14:53:47 -0400

    added less-than-one and fixed template

--- less-than-one.js
+++ less-than-one.js
@@ -1,35 +1,45 @@
-// -----TITLE_HERE
+// -----LESS THAN ONE
 
-// DESCRIPTION
+// Write a function that will take a number, "num", and divide it by 2 until it's less than 1. Once num is less than 1, return num.
 
 // SOLUTION
+
+// recursive solution:
+l=n=>n<1?n:l(n/2)
 
 // TEST CASES
 
 const test = require('./_test');
 const testNum = [1];
 let input, expected;
-const func = 'FUNCTION_NAME_HERE';
+const func = l;
 const lowestTest = 0 || 0;
 const highestTest = 0 || Infinity;
 
 // Test case 1
 input = {
-  ARG_1: 'INPUT_HERE',
+  n: 1,
 };
-expected = 'EXPECTED_HERE';
+expected = 0.5;
 test(func, input, expected, testNum, lowestTest, highestTest);
 
 // Test case 2
 input = {
-  ARG_1: 'INPUT_HERE',
+  n: 25,
 };
-expected = 'EXPECTED_HERE';
+expected = 0.78125;
 test(func, input, expected, testNum, lowestTest, highestTest);
 
 // Test case 3
 input = {
-  ARG_1: 'INPUT_HERE',
+  n: 66,
 };
-expected = 'EXPECTED_HERE';
+expected = 0.515625;
+test(func, input, expected, testNum, lowestTest, highestTest);
+
+// Test case 4
+input = {
+  n: 87,
+};
+expected = 0.6796875;
 test(func, input, expected, testNum, lowestTest, highestTest);
